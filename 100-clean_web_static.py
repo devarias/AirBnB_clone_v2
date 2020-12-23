9c0ce8a1177c26e9e8349153c0bbe3827f449b3f
devarias <gedafu2005@gmail.com>
2020-12-23 05:05:37 +0000

    AdvTask0

---
+++ 100-clean_web_static.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Fabric script (based on the file 3-deploy_web_static.py)
+that deletes out-of-date archives, using the function do_clean"""
+
+from fabric.contrib import files
+from fabric.api import env, put, run, local, lcd, cd
+from os.path import exists, isdir
+from datetime import datetime
+
+env.hosts = ['34.74.218.65', '35.190.174.10']
+
+
+def do_clean(number=0):
+    """function to that delete out-of-date archives"""
+    if int(number) < 2:
+        number = "3"
+    else:
+        number = "4"
+    with lcd("versions"):
+        local("ls -1t | tail -n +{} > files.tmp".format(number))
+        local("rm -rf `cat files.tmp`")
+        local("rm files.tmp")
+    with cd("/data/web_static/releases/"):
+        if number == "3":
+            number = "1"
+        else:
+            number = "3"
+        run("ls -1t | grep web_static_ | tail -n +" +
+            number + " | xargs -I {} rm -rf -- {}")
