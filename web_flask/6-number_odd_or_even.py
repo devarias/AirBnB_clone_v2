f40362b755ebe1f0f2b99c3de894a6d3b0d3d80b
devarias <gedafu2005@gmail.com>
2021-01-19 09:22:07 -0500

    Odd or even?

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -28,35 +28,41 @@ def home():
     """Displays 'Hello HBNB!'"""
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """Displays 'HBNB'"""
     return "HBNB"
+
 
 @app.route("/c/<name>", strict_slashes=False)
 def c(name):
     """Displays 'C' {} name"""
     return "C {}".format(name.replace('_', ' '))
 
+
 @app.route("/python/", strict_slashes=False)
-@app.route("/python/(<text>)", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
 def python(text="is_cool"):
     """Displays 'Python' {} text"""
     return "Python {}".format(text.replace('_', ' '))
 
+
 @app.route("/number/<int:n>", strict_slashes=False)
 def number(n):
-    """Displays 'Python' {} text"""
+    """Displays '{number} is a number"""
     return "{} is a number".format(n)
+
 
 @app.route("/number_template/<int:n>", strict_slashes=False)
 def number_template(n):
-    """Displays 'Python' {} text"""
+    """Displays a HTML page"""
     return render_template('5-number.html', n=n)
+
 
 @app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
 def number_odd_or_even(n):
-    """Displays 'Python' {} text"""
+    """Displays a HTML page"""
     return render_template('6-number_odd_or_even.html', n=n)
 
 if __name__ == "__main__":
