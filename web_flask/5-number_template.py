ae0c3dfc39e3c541bd38919fe0fa819f1fc8123e
devarias <gedafu2005@gmail.com>
2021-01-19 09:20:00 -0500

    Number template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -25,30 +25,35 @@ def home():
     """Displays 'Hello HBNB!'"""
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """Displays 'HBNB'"""
     return "HBNB"
+
 
 @app.route("/c/<name>", strict_slashes=False)
 def c(name):
     """Displays 'C' {} name"""
     return "C {}".format(name.replace('_', ' '))
 
+
 @app.route("/python/", strict_slashes=False)
-@app.route("/python/(<text>)", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
 def python(text="is_cool"):
     """Displays 'Python' {} text"""
     return "Python {}".format(text.replace('_', ' '))
 
+
 @app.route("/number/<int:n>", strict_slashes=False)
 def number(n):
-    """Displays 'Python' {} text"""
+    """Displays '{number} is a number"""
     return "{} is a number".format(n)
+
 
 @app.route("/number_template/<int:n>", strict_slashes=False)
 def number_template(n):
-    """Displays 'Python' {} text"""
+    """Displays a HTML page"""
     return render_template('5-number.html', n=n)
 
 if __name__ == "__main__":
