1b647e202aa4aaaca4b8dac8bf6bdad178d8e56e
devarias <gedafu2005@gmail.com>
2021-01-19 09:19:31 -0500

    Python is cool!

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -19,18 +19,21 @@ def home():
     """Displays 'Hello HBNB!'"""
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """Displays 'HBNB'"""
     return "HBNB"
+
 
 @app.route("/c/<name>", strict_slashes=False)
 def c(name):
     """Displays 'C' {} name"""
     return "C {}".format(name.replace('_', ' '))
 
+
 @app.route("/python/", strict_slashes=False)
-@app.route("/python/(<text>)", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
 def python(text="is_cool"):
     """Displays 'Python' {} text"""
     return "Python {}".format(text.replace('_', ' '))
