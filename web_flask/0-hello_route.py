9c82e109f95d3e8fe20725d3619c13ca8dff839f
devarias <gedafu2005@gmail.com>
2021-01-19 13:08:03 -0500

    Trying

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,11 +9,10 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def home():
-    """Displays 'Hello HBNB!'"""
+    """Print hello HBNB"""
     return "Hello HBNB!"
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
