c31a0d3dd0b965f53456d88ff23a426edf8c8a07
devarias <gedafu2005@gmail.com>
2021-01-19 09:19:43 -0500

    Is it a number?

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -22,15 +22,18 @@ def home():
     """Displays 'Hello HBNB!'"""
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """Displays 'HBNB'"""
     return "HBNB"
 
+
 @app.route("/c/<name>", strict_slashes=False)
 def c(name):
     """Displays 'C' {} name"""
     return "C {}".format(name.replace('_', ' '))
+
 
 @app.route("/python/", strict_slashes=False)
 @app.route("/python/(<text>)", strict_slashes=False)
@@ -38,9 +41,10 @@ def python(text="is_cool"):
     """Displays 'Python' {} text"""
     return "Python {}".format(text.replace('_', ' '))
 
+
 @app.route("/number/<int:n>", strict_slashes=False)
 def number(n):
-    """Displays 'Python' {} text"""
+    """Displays '{number} is a number"""
     return "{} is a number".format(n)
 
 if __name__ == "__main__":
