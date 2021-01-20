f85cb91a58f70fcae91c433c332879db41e48033
devarias <gedafu2005@gmail.com>
2021-01-20 07:20:22 -0500

    Cities by states

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 @app.route("/cities_by_states", strict_slashes=False)
 def cities_by_states_list():
     """'/cities_by_states': 'Display a HTML page: (inside the tag BODY)'"""
-    return render_template('8-cities_by_states.html', states=storage.all("State"))
+    return render_template('8-cities_by_states.html', sts=storage.all("State"))
 
 
 @app.teardown_appcontext
